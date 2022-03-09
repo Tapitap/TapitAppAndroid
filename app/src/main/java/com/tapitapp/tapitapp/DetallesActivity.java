40ca6e16203707f207c68f8b75b9c885704c29e3
josgonrod7 <josegonzarod@gmail.com>
2022-03-09 18:28:34 +0100

    vista de productos terminadas

--- app/src/main/java/com/tapitapp/tapitapp/DetallesActivity.java
+++ app/src/main/java/com/tapitapp/tapitapp/DetallesActivity.java
@@ -3,12 +3,17 @@ package com.tapitapp.tapitapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DetallesActivity extends AppCompatActivity {
 
+    TextView txtNombre;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalles);
+        txtNombre=(TextView) findViewById(R.id.txtNombre);
+
+        ;
     }
 }
